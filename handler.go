04073c1ba48773f51c921cf061f696d6443d036e
Christopher Nyberg <christopher.nyberg@gmail.com>
2018-10-25 22:07:24 -0700

    Made more modular for easier testing

---
+++ handler.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/go-redis/redis"
+	"github.com/labstack/echo"
+)
+
+type apiResponse struct {
+	Allow bool `json:"allow"`
+}
+
+type requestBody struct {
+	User string `json:"user"`
+	URL  string `json:"url"`
+}
+
+func createThrottleKey(user *requestBody) string {
+	// Get the current minute
+	t := time.Now()
+	currentMinute := t.Minute()
+
+	// Create a key
+	return user.User + "-" + user.URL + ":" + strconv.Itoa(currentMinute)
+
+}
+
+func throttleHandler(client *redis.Client) func(c echo.Context) error {
+
+	return func(c echo.Context) error {
+		// Capture the request body
+		u := new(requestBody)
+
+		// Return a failed status if you can't coerce
+		if err := c.Bind(u); err != nil {
+			return c.String(http.StatusBadRequest, "Incorrect POST body")
+		}
+
+		redisKey := createThrottleKey(u)
+
+		// Increment and set timeout
+		pipe := client.TxPipeline()
+		incr := pipe.Incr(redisKey)
+		pipe.Expire(redisKey, time.Second*59)
+		// Execute the pipeline
+		_, err := pipe.Exec()
+		if err != nil {
+			return c.String(http.StatusInternalServerError, "Internal server error")
+		}
+		currentCount := incr.Val()
+
+		// TODO: acquire thresholds from another service
+		if currentCount <= 5 {
+			return c.JSON(http.StatusOK, apiResponse{true})
+		}
+		// Otherwise, you are throttled
+		return c.JSON(http.StatusOK, apiResponse{false})
+
+	}
+}
--- server.go
+++ server.go
@@ -1,29 +1,15 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"strconv"
-	"time"
-
 	"github.com/go-redis/redis"
 	"github.com/labstack/echo"
 )
-
-type apiResponse struct {
-	Allow bool `json:"allow"`
-}
-
-type requestBody struct {
-	User string `json:"user"`
-	URL  string `json:"url"`
-}
 
 func mkRt(url string) string {
 	return ("/api/throttling-service/v1" + url)
 }
 
-func main() {
+func makeServer() *echo.Echo {
 	// Connect to Redis
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
@@ -32,41 +18,12 @@ func main() {
 	})
 
 	e := echo.New()
-	e.POST(mkRt("/throttle"), func(c echo.Context) error {
-		// Capture the request body
-		u := new(requestBody)
+	e.POST(mkRt("/throttle"), throttleHandler(client))
 
-		// Return a failed status if you can't coerce
-		if err := c.Bind(u); err != nil {
-			return c.String(http.StatusBadRequest, "Incorrect POST body")
-		}
+	return e
+}
 
-		// Get the current minute
-		t := time.Now()
-		currentMinute := t.Minute()
-
-		// Create a key
-		redisKey := u.User + "-" + u.URL + ":" + strconv.Itoa(currentMinute)
-
-		// Increment and set timeout
-		pipe := client.TxPipeline()
-		incr := pipe.Incr(redisKey)
-		pipe.Expire(redisKey, time.Second*59)
-		// Execute the pipeline
-		_, err := pipe.Exec()
-		if err != nil {
-			return c.String(http.StatusInternalServerError, "Internal server error")
-		}
-		currentCount := incr.Val()
-
-		// TODO: acquire thresholds from another service
-		if currentCount <= 5 {
-			return c.JSON(http.StatusOK, apiResponse{true})
-		} else {
-			return c.JSON(http.StatusOK, apiResponse{false})
-		}
-
-	})
-
+func main() {
+	e := makeServer()
 	e.Logger.Fatal(e.Start(":1323"))
 }
